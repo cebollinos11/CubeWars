e0c4144a0a432bcf0a8b0132e862b73d73a947c1
Luca Salfi <lucasalfi2@gmail.com>
2015-10-30 13:26:22 +0100

    Changes to be committed: 	new file:   Assets/TEST.cs 	new file:   Assets/TEST.cs.meta

---
+++ Assets/TEST.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEST : MonoBehaviour {
+    private GameManager blabla;
+	// Use this for initialization
+	void Start () {
+        GameManager = new GameManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
