7fab1f44f978cef0eaa86677a07ffb81f7f1c6d9
Luca Salfi <lucasalfi2@gmail.com>
2015-10-30 13:27:52 +0100

    Changes to be committed: 	modified:   Assets/TEST.cs

--- Assets/TEST.cs
+++ Assets/TEST.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class TEST : MonoBehaviour {
-    private GameManager blabla;
+    public GameManager blabla;
+    private GameObject *b;
 	// Use this for initialization
 	void Start () {
         GameManager = new GameManager();
+        b =new  GameObject();
 	}
 	
 	// Update is called once per frame
