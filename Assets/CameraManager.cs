dda445117fb2c1022b63c02bb5ab2d3d7797f1a9
cebollinos11 <cebollinos@gmail.com>
2015-11-09 17:59:06 +0100

    fixed coing bug

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -6,10 +6,10 @@ public class CameraManager : MonoBehaviour {
     Vector3 originalPosition;
     Vector3 targetPosition;
     
-    public float speed;
+    
     Vector3 FocusPoint;
     public float y;
-    Vector3 target;
+    //public Vector3 target;
     public float smoothTime = 0.3F;
     private Vector3 velocity = Vector3.zero;
 	// Use this for initialization
@@ -17,18 +17,22 @@ public class CameraManager : MonoBehaviour {
 
         originalPosition = transform.position;     
         FocusPoint = Vector3.zero;
-        target = originalPosition + new Vector3(0, 0, y);
+        
+        //target= originalPosition + new Vector3(0, 0, y);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.LookAt(FocusPoint);
+        //transform.LookAt(FocusPoint);
 
       //transform.position = Vector3.Lerp();
 
-        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-        transform.position = new Vector3(transform.position.x, transform.position.y, originalPosition.z);
+        //transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
+        //transform.position = new Vector3(transform.position.x, transform.position.y, originalPosition.z);
+        transform.position = new Vector3(originalPosition.x + Mathf.Sin(Time.time * smoothTime/2) * y*2, transform.position.y, transform.position.z);
+
+       
 	}
 }
