cc020c8548daff23e4c6b9d1ad869c1c02e291a1
cebollinos11 <cebollinos@gmail.com>
2015-11-09 16:15:44 +0100

    fixed shadow

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+    Vector3 originalPosition;
+    Vector3 targetPosition;
+    public float yDamp;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+
+        originalPosition = transform.position;
+        targetPosition = transform.position + new Vector3(0, -yDamp, 0);
+       
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+      //transform.position = Vector3.Lerp();
+	}
+}
