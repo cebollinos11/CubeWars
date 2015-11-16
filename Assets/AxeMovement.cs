df70b96964d3cad9e36736897f8a21cca01948eb
Fumaloko <lucasalfi2@gmail.com>
2015-11-16 16:07:00 +0100

    Hammers and CPlane added

---
+++ Assets/AxeMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxeMovement : MonoBehaviour {
+    public enum OscillationType
+    {
+        LeftToRight,
+        RightToLeft
+    }
+    public OscillationType oscillationType;
+    private float _forceApplied=0;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        _forceApplied += Time.fixedDeltaTime;
+        if(oscillationType==OscillationType.RightToLeft)
+        transform.Rotate(new Vector3(0,0,Mathf.Sin(_forceApplied)));
+        else
+            if (oscillationType == OscillationType.LeftToRight)
+            transform.Rotate(new Vector3(0, 0, -Mathf.Sin(_forceApplied)));
+    }
+}
