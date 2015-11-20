e56156fbf3750ded9d7b2c46acc7bc99444e670f
cebollinos11 <cebollinos@gmail.com>
2015-11-20 13:05:56 +0100

    added 2 new maps

---
+++ Assets/ButtonDoubleClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonDoubleClick : MonoBehaviour {
+
+
+
+
+    
+    Button buttonMe;
+    // Use this for initialization
+    void Start()
+    {
+        buttonMe = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Start"))
+        {
+            buttonMe.onClick.Invoke();
+        }
+
+
+    }
+}
---
+++ Assets/rotateCPlane.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateCPlane : MonoBehaviour {
+
+    [SerializeField]
+    float RotSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, Time.deltaTime * RotSpeed);
+	}
+}
