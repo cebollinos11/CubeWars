f81bac1b136fcf144ecd7669a979cccf45a3e576
Fumaloko <lucasalfi2@gmail.com>
2015-11-26 17:03:56 +0100

    test pillars

---
+++ Assets/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public float RotationPower;
+    public bool clockwise;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody>().maxAngularVelocity = RotationPower * 1000f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(clockwise)
+        gameObject.transform.Rotate(new Vector3(0,RotationPower*Time.deltaTime,0));
+        else
+            gameObject.transform.Rotate(new Vector3(0, -RotationPower * Time.deltaTime, 0));
+    }
+}
